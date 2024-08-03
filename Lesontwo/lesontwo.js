68761214faccbe56588ea88baa33f68f790756eb
Nekoshotaa <timur.ramaznov@gmail.com>
2024-08-03 15:58:59 +0600

    Add files via upload

---
+++ Lesontwo/lesontwo.js
@@ -0,0 +1,42 @@
+// alert ('01.08.2024')
+
+let d = ('Добро пожаловать на курс Тимур');
+
+console.log(d);
+// Infinity
+let a = Infinity
+console.log(a);
+
+// NaN
+console.log('stroka' / 20);
+
+// Bigint
+let b = BigInt
+console.log(4444444777778888888354523452n);
+
+
+// string
+
+let str1 = 'Onepieace'
+let str2 = "Onepieace"
+let str3 = `is real ${str1}`
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+// Булевой  тип данных
+// true и false
+
+Console.log(20 < 202);
+
+// Null
+
+console.log(null);
+
+// undefined
+
+let c;
+
+console.log(c);
+
